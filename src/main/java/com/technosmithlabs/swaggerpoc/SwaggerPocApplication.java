bdd1cc991c05cc1e7e64735f3af2467acd6a0fc6
technosmithlabs <email@anshulgautam.in>
2020-05-25 16:10:24 +0530

    initialized the project

---
+++ src/main/java/com/technosmithlabs/swaggerpoc/SwaggerPocApplication.java
@@ -0,0 +1,13 @@
+package com.technosmithlabs.swaggerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technosmithlabs/swaggerpoc/SwaggerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technosmithlabs.swaggerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
